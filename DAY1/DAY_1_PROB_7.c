20f636105bf2a5ca9636f17241821ac1495d78e7
Anuja-jagtap <108973938+Anuja-jagtap@users.noreply.github.com>
2022-07-11 23:25:49 +0530

    Create DAY_1_PROB_7.c

---
+++ DAY1/DAY_1_PROB_7.c
@@ -0,0 +1,27 @@
+//Find whether a given 10-digit number is palindrome or not. 
+/*1)include the header file
+2)read the digit from the user
+3)write the condition for divide each number and check whether it is polindrome or not
+4)print the output*/
+
+#include<stdio.h>  
+int main()  
+{
+int n,j,k=0,m;
+  printf("enter the number\n");
+  scanf("%d",&n);
+  m=n;
+  loop:if(n>0)
+{
+      j=n%10;
+      k=k*10+j;
+      n=n/10;
+  
+  goto loop;
+ }
+ if(m==k)
+ printf("number is polindrome %d\n",k);
+ else
+ printf("number not is polindrome %d\n");
+    return 0;
+}
