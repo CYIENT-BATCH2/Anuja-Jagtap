40a60de77eb48a92547eacbf4caea391ad691538
Anuja-jagtap <108973938+Anuja-jagtap@users.noreply.github.com>
2022-07-12 14:19:59 +0530

    Create DAY_1_PROB_9.c

---
+++ DAY1/DAY_1_PROB_9.c
@@ -0,0 +1,13 @@
+//Implement basic logic gates using C (AND, OR, NOT). 
+/*1)include the header file
+2)read the digit from the user
+3)write the condition by checking high and low
+4)print the output*/
+
+#include <stdio.h>
+
+int main() {
+   unsigned a,b,c,d,e,f;
+    scanf("%d %d",&a,&b);
+printf("c=%d\nd=%d\nf=%d",c=a&&b,d=a||b,f=!a);
+}
