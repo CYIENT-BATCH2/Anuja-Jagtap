fd8328ceded5d7e7b4a8cb5d90514c6646a0cb2c
Anuja-jagtap <108973938+Anuja-jagtap@users.noreply.github.com>
2022-07-13 11:58:31 +0530

    Create DAY_3_PROB_4.c

---
+++ DAY3/DAY_3_PROB_4.c
@@ -0,0 +1,10 @@
+//Convert decimal number 204 to hex value, binary value and print the same
+
+#include <stdio.h>
+int main()
+{
+ long int num;
+ printf("Enter any decimal number:");
+ scanf("%d",&num);
+ printf("Hexadecimal number is:%x",num);
+}
